795a1b0cd24adabb43a3e2d5fdc5d9793f9c5599
Matt Policastro <matt@mattpolicastro.com>
2016-02-17 12:41:24 -0500

    Removed debugging-call to warning()

--- R/core.R
+++ R/core.R
@@ -171,7 +171,6 @@ rga$methods(
             names(ga.data.df) <- ga.headers$name
 
             if (isSampled) {
-              warning("this was sampled")
               attr(ga.data.df, "ga:containsSampledData") <- TRUE
               attr(ga.data.df, "ga:sampleSize") <- as.numeric(ga.data$sampleSize)
               attr(ga.data.df, "ga:sampleSpace") <- as.numeric(ga.data$sampleSpace)
